ceabcb69ebe5d0dfb460685c3b56ab824a0b48f2
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-25 17:06:47 -0300

    contact route config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicialComponent } from './pages/inicial/inicial.component'
 import { QuemsomosComponent } from './pages/quemsomos/quemsomos.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'sobre',
     component: QuemsomosComponent
+  },
+  {
+    path: 'contato',
+    component: ContatoComponent
   }
 ];
 
