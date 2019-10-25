aaee026a525bfbd2457328be6433530df2760916
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-25 16:29:02 -0300

    project start

--- src/app/components/main/app.component.ts
+++ src/app/components/main/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<p>app</p>'
 })
 export class AppComponent {
   title = 'Routes-Project';
